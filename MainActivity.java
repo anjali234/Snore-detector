e9a3b715b2c7ae2eb040fcae9c611a2a86929f56
anjali234 <30770861+anjali234@users.noreply.github.com>
2017-08-29 00:54:17 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,188 +1,155 @@
 package com.example.user.snoredetector;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-
-
-
+import android.widget.TextView;
+import android.widget.Toast;
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = "AudioRecordTest";
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
-    private static String mFileName = null;
+import static android.graphics.Color.GREEN;
+import static android.graphics.Color.RED;
 
-    private RecordButton mRecordButton = null;
-    private MediaRecorder mRecorder = null;
-
-    private PlayButton mPlayButton = null;
-    private MediaPlayer mPlayer = null;
-
-    // Requesting permission to RECORD_AUDIO
-    private boolean permissionToRecordAccepted = false;
-    private String[] permissions = {Manifest.permission.RECORD_AUDIO};
-
+public class SnoreDetector extends AppCompatActivity {
+    private CDrawer.CDrawThread mDrawThread;
+    private CDrawer mdrawer;
+    private Boolean m_bStart = Boolean.valueOf(false);
+    private Boolean recording;
+    private CSampler sampler;
+    public static int DETECT_SNORE = 0;
+    TextView result;
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_RECORD_AUDIO_PERMISSION:
-                permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                break;
-        }
-        if (!permissionToRecordAccepted) finish();
-
-    }
-
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snore_detector);
+        mdrawer = (CDrawer) findViewById(R.id.drawer);
+        m_bStart = Boolean.valueOf(false);
+        result = (TextView)findViewById(R.id.result);
+        while (true)
+        {
+            recording = Boolean.valueOf(false);
+            run();
+            System.out.println("mDrawThread NOT NULL");
+            System.out.println("recorder NOT NULL");
+            return;
         }
     }
 
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
+    @Override
+    protected void onPause() {
+        System.out.println("onpause");
+        sampler.SetRun(Boolean.valueOf(false));
+        mDrawThread.setRun(Boolean.valueOf(false));
+        sampler.SetSleeping(Boolean.valueOf(true));
+        mDrawThread.SetSleeping(Boolean.valueOf(true));
+        Boolean.valueOf(false);
+        super.onPause();
+    }
+    @Override
+    protected void onRestart()
+    {
+        m_bStart = Boolean.valueOf(true);
+        System.out.println("onRestart");
+        super.onRestart();
     }
 
-    private void startPlaying() {
-        mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.prepare();
-            mPlayer.start();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-    }
-
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
-    }
-
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            mRecorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-
-        mRecorder.start();
-    }
-
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
-    }
-
-    class RecordButton extends android.support.v7.widget.AppCompatButton {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
-                }
-                mStartRecording = !mStartRecording;
+    public void onStartClick(View view) throws IOException{
+        int i = 0;
+        while (true)
+        {
+            if ((sampler.GetDead2().booleanValue()) && (mdrawer.GetDead2().booleanValue()))
+            {
+                System.out.println(sampler.GetDead2() + ", " + mdrawer.GetDead2());
+                sampler.Restart();
+                if (!m_bStart.booleanValue())
+                    mdrawer.Restart(Boolean.valueOf(true));
+                sampler.SetSleeping(Boolean.valueOf(false));
+                mDrawThread.SetSleeping(Boolean.valueOf(false));
+                m_bStart = Boolean.valueOf(false);
+                return;
             }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
-    }
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
+            try
+            {
+                Thread.sleep(500L);
+                System.out.println("Hang on..");
+                i++;
+                if (!sampler.GetDead2().booleanValue())
+                    System.out.println("sampler not DEAD!!!");
+                if (!mdrawer.GetDead2().booleanValue())
+                {
+                    System.out.println("mDrawer not DeAD!!");
+                    mdrawer.SetRun(Boolean.valueOf(false));
                 }
-                mStartPlaying = !mStartPlaying;
+                if (i <= 4)
+                    continue;
+                mDrawThread.SetDead2(Boolean.valueOf(true));
             }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
+            catch (InterruptedException localInterruptedException)
+            {
+                localInterruptedException.printStackTrace();
+            }
         }
     }
 
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-        // Record to the external cache directory for visibility
-        mFileName = getExternalCacheDir().getAbsolutePath();
-        mFileName += "/audiorecordtest.3gp";
-
-        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
-
-        LinearLayout ll = new LinearLayout(this);
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        setContentView(ll);
+    public void onStopClick(View view) throws IOException {
+        sampler.SetRun(Boolean.valueOf(false));
+        mDrawThread.setRun(Boolean.valueOf(false));
+        sampler.SetSleeping(Boolean.valueOf(true));
+        mDrawThread.SetSleeping(Boolean.valueOf(true));
+        Boolean.valueOf(false);
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mRecorder != null) {
-            mRecorder.release();
-            mRecorder = null;
-        }
+    /**
+     * Recives the buffert from the sampler
+     * @param buffert
+     */
+    public void setBuffer(short[] paramArrayOfShort)
+    {
+        mDrawThread = mdrawer.getThread();
+        mDrawThread.setBuffer(paramArrayOfShort);
+    }
 
-        if (mPlayer != null) {
-            mPlayer.release();
-            mPlayer = null;
+    /**
+     * Called by OnCreate to get everything up and running
+     */
+    public void run()
+    {
+        try
+        {
+            if (mDrawThread == null)
+            {
+                mDrawThread = mdrawer.getThread();
+            }
+            if (sampler == null)
+                sampler = new CSampler(this);
+            Context localContext = getApplicationContext();
+            Display localDisplay = getWindowManager().getDefaultDisplay();
+            Toast localToast = Toast.makeText(localContext, "Recording Started..", Toast.LENGTH_LONG);
+            localToast.setGravity(48, 0, localDisplay.getHeight() / 8);
+            localToast.show();
+            if (sampler != null){
+                sampler.Init();
+                sampler.StartRecording();
+                sampler.StartSampling();
+                DETECT_SNORE = sampler.detectSnoring();
+                if(DETECT_SNORE==1){
+                    result.setText("SNORING");
+                    result.setTextColor(RED);
+                }
+                else{
+                    result.setText("NOT SNORING");
+                    result.setTextColor(GREEN);
+                }
+            }
+        } catch (NullPointerException e) {
+            Log.e("Main_Run", "NullPointer: " + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
 }
